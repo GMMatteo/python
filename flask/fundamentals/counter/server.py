256f7f331d6fbc6b7bc47471db163283d4daf6eb
Giacomo Matteo <gmatteo@outlook.com>
2021-12-17 15:04:51 -0500

    Update counter Assignment

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -4,21 +4,34 @@ app = Flask(__name__)
 app.secret_key = '24681012'
 
 @app.route('/')
-
 def counter():
     session['count']+=1
     return render_template("index.html", count= session['count'])
+
+# @app.route('/')
+# def refresh():
+#     session['count']+=1
+#     return render_template('index.html', visit= session['count'])
+
+@app.route('/destroy_session')
+def destroy():
+    session['count']=0
+    return redirect('/')
 
 @app.route('/add')
 def add():
     session['count']+=1
     return redirect('/')
 
+@app.route('/increment', methods =["POST"])
+def increment():
+    session['count']+=int(request.form['value'])
+    return redirect('/')
+
 @app.route('/reset')
 def reset():
     session['count']=0
     return redirect('/')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
